52765f3536fa2be6e2046e01c3bdc4fb4afae30a
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-07-13 15:29:29 +0530

    Add files via upload

---
+++ isSameLength.js
@@ -0,0 +1,17 @@
+/*Write a function called “isSameLength”.
+Given two words, “isSameLength” returns
+ whether the given words have the same length.*/
+ function isSameLength (word1,word2) {
+     console.log(word1.length)
+     console.log(word2.length)
+     if(word1.length == word2.length)
+     {
+         console.log(true)
+     }
+    
+     else{
+         console.log(false)
+     }
+
+ }
+ isSameLength("maris","siram")
