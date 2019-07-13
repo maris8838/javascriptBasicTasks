4af402b5916c1d00f4cdef77edf2f19389faf7c8
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-07-13 16:29:24 +0530

    Add files via upload

---
+++ lastElementInArray.js
@@ -0,0 +1,13 @@
+/*Write a function called “getLastElement”.
+Given an array, “getLastElement” returns the last element of the given array.
+ If the given array has a length of 0, it should return ‘undefined’.*/
+function getLastElement (array) {
+if(array.length!=0)
+{
+    console.log(array[3]);
+}else
+{
+    console.log(undefined);
+}
+}
+getLastElement([]);
