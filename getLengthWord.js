0b5f7ed1dacc574d33deecfc8132ab9db51c6898
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-07-13 15:08:57 +0530

    Add files via upload

---
+++ getLengthWord.js
@@ -0,0 +1,8 @@
+// getting word length:
+
+/*Write a function called “getLengthOfWord”.
+Given a word, “getLengthOfWord” returns the length of the given word.*/
+function getLengthOfWord (word) {
+console.log(word.length);
+}
+getLengthOfWord("maris")
