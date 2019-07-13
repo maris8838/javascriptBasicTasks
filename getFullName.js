db53e2506503cf704df0917430772b8791f55ba4
M MarisUlaganathan <51428822+maris8838@users.noreply.github.com>
2019-07-13 13:23:34 +0530

    Add files via upload

---
+++ getFullName.js
@@ -0,0 +1,10 @@
+/*
+write a function called "getFullName" .
+given a first and last names ,
+ "getFullName" returns a single string with the
+  given first and last names seperated by a single space.
+*/
+function getFullName (firstName,lastName) {
+    console.log(firstName+" "+lastName)
+}
+getFullName("maris","ulaganathan");
